a5204ddc11a29731de381ade72f05d305540534f
szemanuel <81386019+szemanuel@users.noreply.github.com>
2023-05-11 15:16:12 -0300

    4 files modified, 4 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./style.css";
+import styles from './tailwind.css';
+import 'tailwindcss/tailwind.css';
+
 
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1 className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">Hello StackBtz!</h1>
+      <p className="font-bold text-xl mb-2"> formato </p>
     </div>
   );
 }
---
+++ src/components/Title.jsx
@@ -0,0 +1,8 @@
+const Title = () => {
+  return(
+    <h1 className="text 5x1 font-anton font-bold tracking-widest"> ToDo Apps </h1>
+  )
+}
+
+export { Title }
+
