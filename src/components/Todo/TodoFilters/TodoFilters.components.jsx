a84bbe08a94c1aacae760857c05b084de561a506
szemanuel <81386019+szemanuel@users.noreply.github.com>
2023-05-12 14:59:42 -0300

    1 file modified

--- src/components/Todo/TodoFilters/TodoFilters.components.jsx
+++ src/components/Todo/TodoFilters/TodoFilters.components.jsx
@@ -6,16 +6,24 @@ const FiltersContainer = ({ children }) => {
   );
 };
 
-const ItemsLeft = ({ total=0 }) => {
-return(
-<p>
+const ItemsLeft = ({ total = 0 }) => {
+  return <p>{total} items left</p>;
+};
 
-)
-
-
-
-
+const FilterButtonContainer = ( {children} ) =>{
+  return(
+    <div className="flex items-center space-x-2"> {children} </div>
+  )
 }
 
+const FilterButton = ({ action, active, filter}) =>{
+  return(
+    <button className={"hover:text-white cursor-pointer transition-all duration-300 ease-in-out" +
+  (active.toLowerCase().includes(filter.toLowerCase()) ?  "text-blue-400" : "text-gray-400")
+    }>
+      {filter}
+    </button>
+  )
+}
 
-export { FiltersContainer };
+export { FiltersContainer, ItemsLeft, FilterButtonContainer, FilterButton };
