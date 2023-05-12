a9e842f839f9f2436ac683e1859f2c0fbc58dc07
szemanuel <81386019+szemanuel@users.noreply.github.com>
2023-05-12 10:56:06 -0300

    2 files modified

--- src/App.js
+++ src/App.js
@@ -5,16 +5,15 @@ import 'tailwindcss/tailwind.css';
 import { Title } from './components/Title/Title';
 import { TodoInput } from './components/TodoInput/Todoinput';
 import { TodoList } from './components/TodoList/TodoList';
-
+import { Todo } from './components/Todo/Todo';
 
 export default function App() {
-
   return (
     <div>
       <Title />
       <TodoInput />
       <TodoList>
-        <h2>Hola</h2>
+        <Todo />
       </TodoList>
     </div>
   );
--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -2,7 +2,14 @@ import React from 'react';
 
 const Todo = () => {
   return (
-   
+    <div className="flex items-center justify-between p-4 border-solid">
+      <div className="flex items-center">
+        <span className="border-solid border rounded-full p-3 cursor-pointer"> </span>
+        <p className="pl-3">
+          Todo Item
+          </p>
+      </div>
+    </div>
   );
 };
 
