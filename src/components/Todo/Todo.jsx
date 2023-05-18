a8a6c9d12d7fa42ddc76145c372f1d0b790d2e9d
szemanuel <81386019+szemanuel@users.noreply.github.com>
2023-05-18 16:50:19 -0300

    2 files modified

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -2,25 +2,32 @@ import React from 'react';
 import close from './close-icon.svg';
 import check from './check-icon.svg';
 
-
-const Todo = ({ todo }) => {
+const Todo = ({ todo, handleSetComplete }) => {
   const { title, completed, id } = todo;
 
   return (
     <div className="flex items-center justify-between p-4 border-solid">
       <div className="flex items-center">
         {completed ? (
-          <div className="bg-green-700 p-1 rounded-full cursor-pointer"> 
-          <img className="h4 w-4" src={check} alt="check icon" />
-          
+          <div
+            onClick={() => handleSetComplete(id)}
+            className="bg-green-700 p-1 rounded-full cursor-pointer"
+          >
+            <img className="h4 w-4" src={check} alt="check icon" />
           </div>
         ) : (
-          <div> <span className="border-solid border border-grey-500 rounded-full p-3 cursor-pointer"> </span>
+          <div>
+            {' '}
+            <span
+              onClick={() => handleSetComplete(id)}
+              className="border-solid border border-grey-500 rounded-full p-3 cursor-pointer"
+            >
+              {' '}
+            </span>
           </div>
         )}
 
-        
-        <p className={"pl-3" + (completed && "line-througt")}>{title}</p>
+        <p className={'pl-3' + (completed && 'line-througt')}>{title}</p>
       </div>
       <img
         src={close}
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { Todo } from '../Todo/Todo';
 import { TodoFilters } from '../TodoFilters/TodoFilters';
 
-const TodoList = ({ todos }) => {
+const TodoList = ({ todos, handleSetComplete }) => {
   return (
     <div clasName="flex flex-col mt-7 rounded-lg overflow-hidden shadow-2x1">
       {todos.map((todo) => {
-        return <Todo key={todo.id} todo={todo} />;
+        return <Todo key={todo.id} todo={todo} handleSetComplete={handleSetComplete} />;
       })}
       <TodoFilters />
     </div>
