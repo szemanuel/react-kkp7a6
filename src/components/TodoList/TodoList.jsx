15bd89a25aeec6ffedb9a7e4aeb7b536f2e80c67
szemanuel <81386019+szemanuel@users.noreply.github.com>
2023-05-19 14:25:57 -0300

    1 file modified

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -2,13 +2,38 @@ import React from 'react';
 import { Todo } from '../Todo/Todo';
 import { TodoFilters } from '../TodoFilters/TodoFilters';
 
-const TodoList = ({ todos, handleSetComplete, handleDelete }) => {
+const TodoList = ({
+  todos,
+  handleSetComplete,
+  handleDelete,
+  activefilters,
+  showAllTodos,
+  showActiveTodos,
+  showCompletedTodos,
+  handleClearComplete,
+}) => {
   return (
     <div clasName="flex flex-col mt-7 rounded-lg overflow-hidden shadow-2x1">
       {todos.map((todo) => {
-        return <Todo key={todo.id} todo={todo} handleSetComplete={handleSetComplete} handleDelete={handleDelete} />;
+        return (
+          <Todo
+            key={todo.id}
+            todo={todo}
+            handleSetComplete={handleSetComplete}
+            handleDelete={handleDelete}
+          />
+        );
       })}
-      <TodoFilters />
+      <TodoFilters 
+      activeFilters={activefilters}
+      total={todos.length}
+      showAllTodos={showAllTodos}
+      showActiveTodos={showActiveTodos}
+      showCompletedTodos={showCompletedTodos}
+      handleClearComplete={handleClearComplete}
+   
+      
+      />
     </div>
   );
 };
